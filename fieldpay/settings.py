1f3aba57a08d7f06f96905a9e6c5cbb5f64d5417
zameer123-2397 <apexsaastools@gmail.com>
2026-09-01 14:36:30 +0500

    Update settings.py

--- fieldpay/settings.py
+++ fieldpay/settings.py
@@ -124,5 +124,3 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fieldpay.app@gmail.com'
-EMAIL_HOST_PASSWORD ='pblducalokmrndgh'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
